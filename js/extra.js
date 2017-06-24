874512a652af096fec1dff234f8d4e9cb2d9b276
donkeypagoda <christopherallenstewart@gmail.com>
2017-06-23 21:38:02 -0700

    totals working now

---
+++ js/extra.js
@@ -0,0 +1,41 @@
+// variables
+let orderField = $('.order');
+let subTotal = $('.sub');
+let tax = $(".tax");
+let tot = $(".tot");
+
+// event handlers
+$('.card').on('click', 'a', itemInfo);
+
+
+
+// Functions
+function itemInfo(event){
+  let itemToAdd = $(event.target).parent('.card-action').siblings('.card-content').children('span').text();
+  let itemPrice = $(event.target).parent('.card-action').siblings('.card-content').children('p').text();
+  let itemRow = $('<tr>');
+  let itemName = $('<td>').text(itemToAdd);
+  itemRow.append(itemName);
+  itemRow.append($('<td>').text(itemPrice).attr("class", "rightness"));
+  orderField.append(itemRow);
+  updateSubTotal(itemPrice);
+}
+
+function updateSubTotal(itemPrice){
+  let totes = subTotal.text().slice(1, subTotal.text().length);
+  if (totes === 0.00){
+    subTotal.text(itemPrice);
+    updateTaxAndTotal(subTotal);
+  }
+  else {
+    let newTotes = Number(totes) + Number(itemPrice.slice(1, itemPrice.length))
+    subTotal.text("$" + newTotes.toFixed(2));
+    updateTaxAndTotal(subTotal);
+  }
+}
+
+function updateTaxAndTotal(subTotal){
+  let taxable = subTotal.text();
+  tax.text("$" + (Number(taxable.slice(1, taxable.length)) * 0.096).toFixed(2));
+  tot.text("$" + ((Number(taxable.slice(1, taxable.length)) * 0.096) + (Number(taxable.slice(1, taxable.length)))).toFixed(2));
+}
