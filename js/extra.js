7461dead919fe23be19c61620b091f86265ad71f
donkeypagoda <christopherallenstewart@gmail.com>
2017-06-26 15:14:19 -0700

    word up on my validations yo

--- js/extra.js
+++ js/extra.js
@@ -42,10 +42,21 @@ function updateTaxAndTotal(subTotal){
   tot.text("$" + ((Number(taxable.slice(1, taxable.length)) * 0.096) + (Number(taxable.slice(1, taxable.length)))).toFixed(2));
 }
 
+// I did not do a good job at validation, please tell how to do better, I don't like being suck
 function placeOrder(event) {
   if (orderField.children().text() === ""){
     Materialize.toast("You haven't ordered anything!", 4000);
-    console.log("fuck");
   }
-
+  else if (clientData.children().children().children('input[id="name"]').val() === ""){
+    Materialize.toast("Please fill out your personal information.", 4000);
+  }
+  else if (!/[0-9]{10}/.test(clientData.children().children().children('input[type="tel"]').val())){
+    Materialize.toast("Please enter a valid 10 digit phone number.", 4000);
+  }
+  else if (clientData.children().children().children('textarea').val() === "") {
+    Materialize.toast("Please fill out your personal information.", 4000);
+  }
+  else{
+    Materialize.toast("Thanks for your order!", 4000);
+  }
 }
